4d8574ea2e708a33aa6516e123b0477d10049754
Ryann <ryann.arruda_159@hotmail.com>
2023-10-27 19:44:31 -0300

    Adição de nova função para verificar a existência da pasta com os dados aumentados

--- util.py
+++ util.py
@@ -1,31 +1,37 @@
-from os import listdir
-from typing import List, Tuple
-from PIL import Image
-import numpy as np
-from tqdm import tqdm
+import os
 
-def get_images_names_in_folder(folder_path:str) -> List[str]:
-  image_names: List[str] = []
-  print('> Getting images in ' + folder_path)
+def check_dataset_exist(root_path):
+    if not os.path.exists(root_path + '/Dataset'):
+        raise FileNotFoundError(f"The path to the dataset doesn't exist! Path used: {root_path + '/Dataset'}")
+    
+    if not os.path.exists(root_path + '/Dataset/images'):
+        raise FileNotFoundError(f"The path to the dataset doesn't exist! Path used: {root_path + '/Dataset/images'}")
 
-  for file_name in tqdm(listdir(folder_path), "Getting...", ncols=100):
-    image_names.append(file_name)
+    if not os.path.exists(root_path + '/Dataset/masks'):
+        raise FileNotFoundError(f"The path to the dataset doesn't exist! Path used: {root_path + '/Dataset/masks'}")
+    
+    if not os.listdir(root_path + '/Dataset/images'):
+        raise FileNotFoundError(f"Images not found! Path used: {root_path + '/Dataset/images'}")
+    
+    if not os.listdir(root_path + '/Dataset/masks'):
+        raise FileNotFoundError(f"Masks not found! Path used: {root_path + '/Dataset/masks'}")
 
-  return image_names
+    return True
 
-def get_images_ndarray_in_folder(folder_path:str) -> List[np.ndarray]:
-    return np.asarray([np.asarray(Image.open(folder_path + image_name)) for image_name in get_images_names_in_folder(folder_path)])
+def check_augmented_dataset_exist(root_path):
+    if not os.path.exists(root_path + '/Augmented Dataset'):
+        raise FileNotFoundError(f"The path to the dataset doesn't exist! Path used: {root_path + '/Augmented Dataset'}")
 
-def get_maps_augmented() -> List[Image.Image]:
-  return get_images_ndarray_in_folder('Augmented Dataset/images/') 
+    if not os.path.exists(root_path + '/Augmented Dataset/images'):
+        raise FileNotFoundError(f"The path to the dataset doesn't exist! Path used: {root_path + '/Augmented Dataset/images'}")
 
-def get_masks_augmented() -> List[Image.Image]:
-    return get_images_ndarray_in_folder('Augmented Dataset/masks/')
+    if not os.path.exists(root_path + '/Augmented Dataset/masks'):
+        raise FileNotFoundError(f"The path to the dataset doesn't exist! Path used: {root_path + '/Augmented Dataset/masks'}")
 
-# (Normalization) scale from [0,255] to [-1,1] 
-def downscale_image_pixels(image:np.ndarray) -> np.ndarray:
-  return (image - 127.5) / 127.5
+    if not os.listdir(root_path + '/Augmented Dataset/images'):
+        raise FileNotFoundError(f"Images not found! Path used: {root_path + '/Augmented Dataset/images'}")
 
-# Image and mask normalization
-def downscale_pair_image_pixels(pair_image:Tuple[np.ndarray, np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
-  return (downscale_image_pixels(pair_image[0]), downscale_image_pixels(pair_image[1]))
+    if not os.listdir(root_path + '/Augmented Dataset/masks'):
+        raise FileNotFoundError(f"Masks not found! Path used: {root_path + '/Augmented Dataset/masks'}")
+
+    return True
