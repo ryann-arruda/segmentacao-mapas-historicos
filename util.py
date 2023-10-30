13470be4570885d1c43d1de68526dd0ec7be61d0
Ryann <ryann.arruda_159@hotmail.com>
2023-10-30 13:00:30 -0300

    Criação da função para remover bordas desnecessárias e adição da função para verificar se o caminho para o conjunto de dados aumentado existe

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import os
+from PIL import Image
+import shutil
 
 def check_dataset_exist(root_path):
     if not os.path.exists(root_path + '/Dataset'):
@@ -34,4 +36,26 @@ def check_augmented_dataset_exist(root_path):
     if not os.listdir(root_path + '/Augmented Dataset/masks'):
         raise FileNotFoundError(f"Masks not found! Path used: {root_path + '/Augmented Dataset/masks'}")
 
+    return True
+
+def remove_unnecessary_border(image):
+    img = image.copy()
+    img = img.convert('RGBA')
+
+    aux_image = Image.new('RGBA', img.size, (255,)*4)
+
+    borderless_image = Image.composite(img, aux_image, img)
+
+    return borderless_image.convert(image.mode)
+
+def check_save_path(root_path):
+
+    if os.path.exists(root_path + '/Augmented Dataset'):
+        shutil.rmtree(root_path + '/Augmented Dataset')
+
+    path = root_path + '/Augmented Dataset'
+    os.mkdir(path)
+    os.mkdir(path + '/images')
+    os.mkdir(path + '/masks')
+    
     return True
