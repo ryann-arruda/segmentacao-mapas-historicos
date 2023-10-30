2de149493e9b6e9de2a75d63455e20609d643023
Ryann <ryann.arruda_159@hotmail.com>
2023-10-30 12:58:00 -0300

    Atualização da função para cortar imagens e correção do tamanho da imagem rotacionada
    A função para cortar imagem faz uso da função para remover bordas cinzas. A imagem rotacionada foi redimensionada para o tamanho correto de 256, pois ao rotacionar a imagem seu tamanho aumentava, devido ao grau de rotação utilizado

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,21 +1,8 @@
 import os
-import shutil
 from PIL import Image, ImageEnhance
 from tqdm import tqdm
 
 import util
-
-def check_save_path(root_path):
-
-    if os.path.exists(root_path + '/Augmented Dataset'):
-        shutil.rmtree(root_path + '/Augmented Dataset')
-
-    path = root_path + '/Augmented Dataset'
-    os.mkdir(path)
-    os.mkdir(path + '/images')
-    os.mkdir(path + '/masks')
-    
-    return True
 
 def crop_save_image(image, image_name, path_to_save, cut_size):
     index = 0
@@ -23,7 +10,11 @@ def crop_save_image(image, image_name, path_to_save, cut_size):
         for j in range(0, image.width, cut_size):
             image_name_aux = image_name.replace('.png', str(index) + '.png')
             index += 1
-            image.crop((j, i, j + cut_size, i + cut_size)).save(path_to_save + image_name_aux)
+            
+            cropped_image = image.crop((j, i, j + cut_size, i + cut_size))
+            
+            cropped_borderless_image = util.remove_unnecessary_border(cropped_image)
+            cropped_borderless_image.save(path_to_save + image_name_aux)
 
 def resize(image, size):
     return image.resize((size, size))
@@ -60,7 +51,7 @@ if util.check_dataset_exist(root_path):
     os.chdir(root_path + '/Dataset/images')
     images = os.listdir()
 
-    if check_save_path(root_path):
+    if util.check_save_path(root_path):
         path_to_save_image = root_path + '/Augmented Dataset/images/'
         path_to_save_mask = root_path + '/Augmented Dataset/masks/'
 
@@ -82,8 +73,15 @@ if util.check_dataset_exist(root_path):
             mask_name = image_name.replace('original', 'mask')
             mask = Image.open(os.path.dirname(os.getcwd()) + '/masks/' + mask_name)
 
-            rotate(image, -45).save(path_to_save_image + image_name.replace('.png', 'r.png'))
-            rotate(mask, -45).save(path_to_save_mask + mask_name.replace('.png', 'r.png'))
+            image_rot = rotate(image, -45)
+            image_rot = resize(image_rot, 256)
+            borderless_rot_image = util.remove_unnecessary_border(image_rot)
+            borderless_rot_image.save(path_to_save_image + image_name.replace('.png', 'r.png'))
+
+            mask_rot = rotate(mask, -45)
+            mask_rot = resize(mask_rot, 256)
+            borderless_rot_mask = util.remove_unnecessary_border(mask_rot)
+            borderless_rot_mask.save(path_to_save_mask + mask_name.replace('.png', 'r.png'))
 
             flipHorizontal(image).save(path_to_save_image + image_name.replace('.png', 'f.png'))
             flipHorizontal(mask).save(path_to_save_mask + mask_name.replace('.png', 'f.png'))
